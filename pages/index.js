823ea6e9aaee71e30176769ae5222d8f226862c9
jostein <jostein-skare@hotmail.com>
2022-08-18 13:04:26 +0200

    Added radio buttons for gender choice and added randomized lastname with -son and -dottir based on gendername

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
+
 const socket = io.connect("http://localhost:3001");
 
 export default function Home() {
   const [userData, setUserData] = useState([]);
-  const fetchData = () => {
-    return axios
+  const [gender, setGender] = useState();
+  useEffect(() => {
+    axios
       .get("https://data.ssb.no/api/v0/no/table/10467/")
       .then((response) => setUserData(response.data.variables[0].valueTexts));
-  };
+  }, []);
   console.log(userData);
   const listOfFemaleNames = [];
   const listOfMaleNames = [];
@@ -34,28 +36,71 @@ export default function Home() {
   //   { gender: "female", name: "Katrine" },
   //   { gender: "female", name: "Camilla" },
   // ];
-  const listOfAllNames = listOfMaleNames.concat(listOfFemaleNames);
-  const randomUsername =
-    listOfAllNames[Math.floor(Math.random() * listOfAllNames.length)];
+  // const listOfAllNames = listOfMaleNames.concat(listOfFemaleNames);
 
+  const randomUsername = Math.floor(Math.random() * userData.length);
+  const randomFemaleLastName = Math.floor(
+    Math.random() * listOfFemaleNames.length
+  );
+  const randomFemaleFirstName = Math.floor(
+    Math.random() * listOfFemaleNames.length
+  );
+  const randomMaleLastName = Math.floor(Math.random() * listOfMaleNames.length);
+  const randomMaleFirstName = Math.floor(
+    Math.random() * listOfMaleNames.length
+  );
   const [username, setUsername] = useState([]);
+  const [lastName, setLastName] = useState([]);
+  // setter randomisert fornavn og etternavn basert på om indeksen til randomUsername er jentenavn eller guttenavn
+  useEffect(() => {
+    if (randomUsername <= 999) {
+      setUsername(listOfFemaleNames[randomFemaleFirstName]);
+      setLastName(listOfFemaleNames[randomFemaleLastName] + "dottir");
+    } else {
+      setUsername(listOfMaleNames[randomMaleFirstName]);
+      setLastName(listOfMaleNames[randomMaleLastName] + "son");
+    }
+  }, [userData]);
 
-  console.log(randomUsername, "test3");
+  const handleChange = () => {
+    setUsername(userData[randomUsername]);
+  };
 
-  useEffect(() => {}, []);
-  console.log(username, "henter fra API");
+  const handleRadioChange = (event) => {
+    setGender(event.target.value);
+  };
+
   // useEffect(() => {
   //   Object.keys(randomUsername).map(
   //     (keyName, i) => setUsername(randomUsername[keyName])
   //     // console.log(randomUsername[keyName])
   //   );
   // }, [randomUsername]);
-  console.log(username);
+
   return (
     <div>
-      <a className="App">Chatterom</a>
-      <a className="Brukernavn">Ditt brukernavn er: {username}</a>
-      <button onClick={fetchData}>Generer brukernavn</button>
+      <a className="Chatterom">Chatterom</a>
+      <a className="MiddleText">Ditt brukernavn er:</a>
+      <a className="Brukernavn">
+        {username} {lastName}
+      </a>
+      <button onClick={handleChange}>Generer brukernavn</button>
+      <label>Jente:</label>
+      <input
+        type="radio"
+        name="jente"
+        value="jente"
+        checked={gender === "jente"}
+        onChange={handleRadioChange}
+      />
+      <label>Gutt:</label>
+      <input
+        type="radio"
+        name="gutt"
+        value="gutt"
+        checked={gender === "gutt"}
+        onChange={handleRadioChange}
+      />
     </div>
   );
 }
